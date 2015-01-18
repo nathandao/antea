b6d909dd0ee27244cc96654b4a85761ddb96cf34
Nathan Dao <nathan@exove.fi>
2015-01-18 21:52:54 +0200

    remove unicorn from deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "antea"
-set :repository,  "http://git.guynathan.com/guynathan/an-tea.git"
+set :repository,  "git@git.guynathan.com:guynathan/an-tea.git"
 
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -20,22 +20,3 @@ task :symlink_database_yml do
   run "ln -sfn #{shared_path}/config/database.yml #{release_path}/config/database.yml"
 end
 after "bundle:install", "symlink_database_yml"
-
-namespace :unicorn do
-  desc "Zero-downtime restart of Unicorn"
-  task :restart, except: { no_release: true } do
-    run "kill -s USR2 `cat /tmp/unicorn.antea.pid`"
-  end
-
-  desc "Start unicorn"
-  task :start, except: { no_release: true } do
-    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
-  end
-
-  desc "Stop unicorn"
-  task :stop, except: { no_release: true } do
-    run "kill -s QUIT `cat /tmp/unicorn.antea.pid`"
-  end
-end
-
-after "deploy:restart", "unicorn:restart"
