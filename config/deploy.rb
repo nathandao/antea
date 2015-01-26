156812451415e6dc667af4ca8032475acbe04f71
Nathan Dao <nathan@exove.fi>
2015-01-27 05:18:49 +0700

    add sidekiq and unicorn to mina

--- config/deploy.rb
+++ config/deploy.rb
@@ -71,6 +71,9 @@ task :deploy => :environment do
     to :launch do
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+
+      invoke :'sidekiq:restart'
+      invoke :'unicorn:restart'
     end
   end
 end
