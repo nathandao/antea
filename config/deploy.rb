accc102ce92fce2e9cfe4dae088c4e5f02b66fc3
Nathan Dao <nathan@exove.fi>
2015-01-27 05:21:23 +0700

    modify mina deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,10 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
+require 'mina/rbenv'
+require 'mina_sidekiq/tasks'
+require 'mina/unicorn'
+
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
