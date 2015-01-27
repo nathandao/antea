3e50d6318f2af5ea3c6e9de2c1e634df10ae20a7
Nathan Dao <nathan@exove.fi>
2015-01-27 07:17:49 +0700

    update unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,48 +1,32 @@
-# config/unicorn.rb
-# Set environment to development unless something else is specified
-env = ENV["RAILS_ENV"] || "production"
+# Set your full path to application.
+app_dir = File.expand_path('../../', __FILE__)
+shared_dir = File.expand_path('../../../shared/', __FILE__)
 
-# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete documentation.
+# Set unicorn options
 worker_processes 2
-
-# listen on both a Unix domain socket and a TCP port,
-# we use a shorter backlog for quicker failover when busy
-listen "/home/deployer/antea/shared/pids/antea.socket", backlog: 64
-
-# Preload our app for more speed
 preload_app true
-
-# nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
-pid "/home/deployer/antea/shared/pids/unicorn.pid"
+# Fill path to your app
+working_directory app_dir
 
-# Production specific settings
-if env == "production"
-  # Help ensure your application will always spawn in the symlinked
-  # "current" directory that Capistrano sets up.
-  working_directory "/home/deployer/antea/current"
+# Set up socket location
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
 
-  # feel free to point this anywhere accessible on the filesystem user 'spree'
-  shared_path = "/home/deployer/antea/shared"
+# Loging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
-  stderr_path "#{shared_path}/log/unicorn.stderr.log"
-  stdout_path "#{shared_path}/log/unicorn.stdout.log"
-end
+# Set master PID location
+pid "#{shared_dir}/pids/unicorn.pid"
 
 before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
-
-  # Before forking, kill the master process that belongs to the .oldbin PID.
-  # This enables 0 downtime deploys.
-  old_pid = "/home/deployer/antea/shared/pids/unicorn.pid.oldbin"
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
       # someone else did our job for us
     end
@@ -50,14 +34,9 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-  # the following is *required* for Rails + "preload_app true"
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-  end
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
 
-  # if preload_app is true, then you may also want to check and
-  # restart any other shared sockets/descriptors such as Memcached,
-  # and Redis.  TokyoCabinet file handles are safe to reuse
-  # between any number of forked children (assuming your kernel
-  # correctly implements pread()/pwrite() system calls)
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{app_dir}/Gemfile"
 end
